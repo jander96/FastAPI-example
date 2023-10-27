4b56334b86198c78c9e60433061bca6df388a563
Jander Laffita <janderdeveloper@gmail.com>
2023-10-27 11:34:12 -0400

    Add: ventas usuario relation 1:1

--- app/db/models.py
+++ app/db/models.py
@@ -2,7 +2,7 @@ from app.db.database import Base
 from sqlalchemy import Column, String, Integer, Boolean, DateTime
 from datetime  import datetime
 from sqlalchemy.schema import ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
 
 class User(Base):
     __tablename__ = "usuario"
@@ -17,10 +17,11 @@ class User(Base):
     correo = Column(String(),unique=True,nullable=True)
     creacion = Column(DateTime(), default=datetime.now())
     estado = Column(Boolean(),default=False)
+    venta = relationship("Venta",backref="usuario")
 
-# class Venta(Base):
-#     __tablename__ = "venta"
-#     id = Column(Integer(), primary_key=True, autoincrement=True)
-#     usuario_id = Column(Integer(),ForeignKey("usuario.id", ondelete= "CASCADE"))
-#     venta = Column(Integer())
-#     ventas_productos = Column(Integer())
+class Venta(Base):
+    __tablename__ = "venta"
+    id = Column(Integer(), primary_key=True, autoincrement=True)
+    usuario_id = Column(Integer(),ForeignKey("usuario.id", ondelete= "CASCADE"))
+    venta = Column(Integer())
+    ventas_productos = Column(Integer())
